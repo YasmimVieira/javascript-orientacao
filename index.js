e0ea3d53f93b9c25b7c659455448241978fbe7a4
Yasmim Barbosa Vieira <yasmim.vieira@aluno.faculdadeimpacta.com.br>
2020-11-27 02:24:59 -0300

    feat: change error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Cliente } from "./Cliente.js";
-import { Gerente } from "./Funcionario/Gerente.js"
-import { Diretor } from "./Funcionario/Diretor.js"
+import { Gerente } from "./Funcionarios/Gerente.js"
+import { Diretor } from "./Funcionarios/Diretor.js"
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
